8cf30e9706439325d548a1dbc36cbf04f7f2ecdd
Joe <Joe.pettit91@gmail.com>
2022-11-02 19:43:09 -0700

    Removing code scaffolding
    - Currently commented, might need for later

--- Client/ClientWindow.java
+++ Client/ClientWindow.java
@@ -1,48 +1,48 @@
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+// import java.net.InetAddress;
+// import java.net.ServerSocket;
+// import java.net.Socket;
+// import java.net.UnknownHostException;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+// import java.io.IOException;
+// import java.io.ObjectInputStream;
+// import java.io.ObjectOutputStream;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+// import javax.swing.JOptionPane;
+// import javax.swing.JTextArea;
 
-public class ClientWindow extends JFrame {
+// public class ClientWindow extends JFrame {
 
-    // String serverAddress;
+//     // String serverAddress;
 
-    // JTextArea serverMessages;
-    JPanel gamePanel;
-    Socket socket;
+//     // JTextArea serverMessages;
+//     JPanel gamePanel;
+//     Socket socket;
 
-    public ClientWindow() {
+//     public ClientWindow() {
         
-        setSize(420, 420);
-        setTitle("X-Tank Client");
-        setResizable(false);
-        gamePanel = new JPanel();
-        add(gamePanel);
-    }
+//         setSize(420, 420);
+//         setTitle("X-Tank Client");
+//         setResizable(false);
+//         gamePanel = new JPanel();
+//         add(gamePanel);
+//     }
 
-    public void run(String serverAddress) throws UnknownHostException, IOException{
+//     public void run(String serverAddress) throws UnknownHostException, IOException{
         
-        socket = new Socket(serverAddress, 59896);
+//         socket = new Socket(serverAddress, 59896);
         
-        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-        ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());    
+//         ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+//         ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());    
 
-    }
+//     }
 
-    private int IPNotConnected() {
-        return JOptionPane.showConfirmDialog(this, "The Server IP address entered was not located. \nTry again?",
-                "Server Address Not Found",
-                JOptionPane.YES_NO_OPTION);
-    }
-}
+//     private int IPNotConnected() {
+//         return JOptionPane.showConfirmDialog(this, "The Server IP address entered was not located. \nTry again?",
+//                 "Server Address Not Found",
+//                 JOptionPane.YES_NO_OPTION);
+//     }
+// }
