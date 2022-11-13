ca89fc69e7eea9f868c5257dff4cf6966f0de5ac
Joe Pettit <Joe.pettit91@gmail.com>
2022-11-12 17:09:09 -0700

    bypass windows

--- Server/xTankServerLogic/Server.java
+++ Server/xTankServerLogic/Server.java
@@ -19,26 +19,45 @@ public class Server implements Runnable {
 
   @Override
   public void run() {
-    Integer numPlayers = (Integer) JOptionPane.showInputDialog(null,
-        "Server Starting \nEnter total number of players:",
-        "xTank Total Players",
-        JOptionPane.QUESTION_MESSAGE, null, availPlayers, availPlayers[0]);
+    if (false) { // TODO: added for testing
+      Integer numPlayers = (Integer) JOptionPane.showInputDialog(null,
+          "Server Starting \nEnter total number of players:",
+          "xTank Total Players",
+          JOptionPane.QUESTION_MESSAGE, null, availPlayers, availPlayers[0]);
 
-    try (var listener = new ServerSocket(58901)) {
+      try (var listener = new ServerSocket(58901)) {
 
-      Socket socket = new Socket();
-      for (int i = 0; i < numPlayers; i++) {
-        System.out.printf("Waiting for player %d to join...\n\n", i + 1);
-        socket = listener.accept();
-        System.out.printf("Connected to %s \n", socket.getInetAddress().getHostAddress());
-        serverModel.addConnection(new ClientConnection(socket, serverModel));
+        Socket socket = new Socket();
+        for (int i = 0; i < numPlayers; i++) {
+          System.out.printf("Waiting for player %d to join...\n\n", i + 1);
+          socket = listener.accept();
+          System.out.printf("Connected to %s \n", socket.getInetAddress().getHostAddress());
+          serverModel.addConnection(new ClientConnection(socket, serverModel));
+        }
+        System.out.println("Starting Server");
+        serverModel.startConnections();
+
+      } catch (IOException e) {
+        e.printStackTrace();
       }
+    }
 
-      System.out.println("Starting Server");
-      serverModel.startConnections();
+    else { // TODO: remove added for testing
+      try (var listener = new ServerSocket(58901)) {
 
-    } catch (IOException e) {
-      e.printStackTrace();
+        Socket socket = new Socket();
+        for (int i = 0; i < 2; i++) {
+          System.out.printf("Waiting for player %d to join...\n\n", i + 1);
+          socket = listener.accept();
+          System.out.printf("Connected to %s \n", socket.getInetAddress().getHostAddress());
+          serverModel.addConnection(new ClientConnection(socket, serverModel));
+        }
+        System.out.println("Starting Server");
+        serverModel.startConnections();
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
   }
